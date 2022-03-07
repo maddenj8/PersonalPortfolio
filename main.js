d88c469e1efae71ec9e0e858960d9114a8708180
Jason Madden <jason.madden123@gmail.com>
2022-03-07 21:12:48 +0000

    got the emblem disc set up

--- main.js
+++ main.js
@@ -4,30 +4,35 @@ import * as THREE from 'three';
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 const scene = new THREE.Scene();
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg')
+    canvas: document.querySelector('#bg'),
+    // alpha: true
   })
-
+  
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight*0.9);
-camera.position.setZ(15);
+camera.position.set(0, 0, 25)
 
 renderer.render(scene, camera);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(1, 1, 1);
+pointLight.position.set(3, 10, 10);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshNormalMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-cube.position.set(0, 0, 0)
-scene.add( cube );
+const geometry = new THREE.CylinderGeometry(5, 5, 1.8, 40, 1 );
+const material = new THREE.MeshPhongMaterial({
+  color: 0x0f3f23
+});
+const disc = new THREE.Mesh( geometry, material );
+
+disc.position.set(6, 0, 0)
+disc.rotation.x = 190 ;
+scene.add( disc );
 
 var controls = new OrbitControls(camera, renderer.domElement);
 
